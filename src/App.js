3b18a2dd6178ceb634b45152be8f4bed24871d18
Ashish Mishra <ashishmishra707058@gmail.com>
2025-11-17 10:13:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const {adminAuth,userAuth} = require('./middleware/auth');
+const app = express();
+
+
+app.use("/admin",adminAuth);
+app.use("/user", userAuth,
+);
+ 
+app.get("/admin/dashboard", (req, res) => {
+  res.send('Welcome to the admin dashboard!');
+});
+
+app.get("/user/dashboard", (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get("/user/delete",(req,res)=>{
+  res.send("this is resp 2nd")
+
+})
+// app.get("/dashboard", (req, res) => {
+//   res.send('Welcome to the dashboard!');
+// });
+
+// app.get("/profile", (req, res) => {
+//   res.send('This is your profile page.');
+// }); 
+//
+  // Exploring POST and DELETE methods
+// app.post("/user", (req, res) => {
+//   res.send('post User created successfully.');
+// });
+// app.delete("/user", (req, res) => {
+//   res.send('User deleted successfully.');
+// }); 
+
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//   console.log(req.params);
+//   res.send({firstNaame:"Ashish", lastName:"Kumar" });
+// });
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const adminAuth = (req, res, next) => {
+  console.log("Middleware for /admin");
+  const Token = "abc";
+  const isAdminAuthenticated = Token === "abc"; // Simulated authentication check
+  if (!isAdminAuthenticated){
+    return res.status(403).send("Access denied");
+  }
+  else{    
+    next();
+  }
+}
+
+const userAuth =(req, res, next) => {
+  console.log("Middleware for /user");
+  const Token = "xyz";
+  const isUserAuthenticated = Token === "xyz"; // Simulated authentication check
+  if (!isUserAuthenticated){
+    return res.status(403).send("Access denied");
+  }
+  else{
+    next();
+  }
+}
+module.exports = { adminAuth, userAuth };
